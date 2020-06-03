3602d47bc4408e56b5173c6f1f72c22dc11229db
John Davey <jdnetsol@gmail.com>
2020-06-03 13:46:04 +0100

    pre typography plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,20 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
+import Layout from "../components/layout"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+    {/* <div style={{ color: `purple` }}> */}
+    <div>
+      <noscript>This site is best enjoyed with javascript enabled</noscript>
+      <Link to="/contact/">Contact</Link> ** 
+      <Link to="/about-css-modules/">About CSS Modules</Link>
+      <Header headerText="Hello Gatsby!" />
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+    </Layout>
+  )
 }
