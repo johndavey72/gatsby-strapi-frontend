1cff3924384bdfc450e519d24c804cbb7a695ea7
John Davey <jdnetsol@gmail.com>
2020-06-04 16:42:48 +0100

    added gatsby source plugin for filesystem and graphql queries

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,12 +16,18 @@
 //   ],
 // }
 
-
 module.exports = {
   siteMetadata: {
     title: `Pandas Eating Lots`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
